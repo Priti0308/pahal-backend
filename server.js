ea9caf04e5be2ba55d95bb61e02cce68bdc01d6c
Priti Kadam <pritikadam0308@gmail.com>
2025-09-21 20:31:13 +0530

    package 2

--- server.js
+++ server.js
@@ -62,3 +62,4 @@ app.use((err, req, res, next) => {
 // --- Start Server ---
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+module.exports = app;
